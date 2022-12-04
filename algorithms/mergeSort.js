0a8d92c1b73bb63c469dd59c076c770c0e1559b3
Konstantin Nikolaev <konstantinnikolaev@Konstantins-MacBook-Pro.local>
2022-12-03 17:32:58 -0800

    Merge Sort

---
+++ algorithms/mergeSort.js
@@ -0,0 +1,40 @@
+function splitArray(arr) {
+  // check length to stop recursion
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const splitIndex = Math.floor(arr.length / 2);
+
+  const leftSide = splitArray(arr.slice(0, splitIndex));
+  const rightSide = splitArray(arr.slice(splitIndex));
+
+  return merge(leftSide, rightSide);
+}
+
+function merge(leftSide, rightSide) {
+  const merged = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < rightSide.length || j < leftSide.length) {
+    const rightValue = rightSide[i];
+    const leftValue = leftSide[j];
+
+    if (i >= rightSide.length) {
+      merged.push(leftValue);
+      j++;
+    } else if (j >= leftSide.length) {
+      merged.push(rightValue);
+      i++;
+    } else if (rightValue < leftValue) {
+      merged.push(rightValue);
+      i++;
+    } else {
+      merged.push(leftValue);
+      j++;
+    }
+  }
+
+  return merged;
+}
