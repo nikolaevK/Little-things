1d8c8638c09d618f0b217b89c346346a8817a416
Konstantin Nikolaev <you@example.com>
2023-02-20 17:55:16 -0800

    selection sort

---
+++ algorithms/selectionSort.js
@@ -0,0 +1,24 @@
+// Similar to bubble sort but instead of pushing max values to the end of the array
+// This algorithm tracks min value and swaps it with beginning value in the array
+// Per one iteration single min value is found and moved to the beginning of the array
+
+// Better than bubble sort only when need to minimize memory usage by decreasing amounts of swaps
+function selectionSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  for (let i = 0; i < arr.length; i++) {
+    let min = arr[i];
+
+    for (let j = 0 + i; j < arr.length; j++) {
+      if (min > arr[j]) {
+        min = arr[j];
+      }
+    }
+    // swap only if i and index of min value are different to avoid extra steps
+    if (i !== arr.indexOf(min)) {
+      arr[arr.indexOf(min)] = arr[i];
+      arr[i] = min;
+    }
+  }
+  return arr;
+}
